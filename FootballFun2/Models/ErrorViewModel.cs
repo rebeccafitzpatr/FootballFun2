80db96a6c0e2510c170edfaa51ec6b136af6c1f7
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2025-02-03 18:31:19 +1300

    Add project files.

---
+++ FootballFun2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballFun2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
